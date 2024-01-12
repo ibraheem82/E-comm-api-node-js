34d38d1c5b4f830a76e1e679f76c50eda9eac496
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-12 18:02:00 +0100

    **feat(app):** Integrate user routes into Express application.

--- app/app.js
+++ app/app.js
@@ -2,8 +2,11 @@ import express from 'express';
 import dotenv from "dotenv";
 dotenv.config();
 import dbConnect from '../config/dbConnect.js';
+import userRoutes from '../routes/usersRoute.js';
 // * Database Connection.
 dbConnect();
 const app = express();
+
+app.use('/', userRoutes)
 
 export default app;
