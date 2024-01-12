2155244b012ba9acaa9e239ef30d6f66eb90966e
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-12 17:51:32 +0100

    **feat(app):** Set up Express application and establish database connection.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 import express from 'express';
-
+import dotenv from "dotenv";
+dotenv.config();
+import dbConnect from '../config/dbConnect.js';
+// * Database Connection.
+dbConnect();
 const app = express();
 
 export default app;
