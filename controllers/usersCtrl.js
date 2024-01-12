c689d90619039bac6125021ee4fcdab0f17f7f72
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-12 18:29:53 +0100

    **feat(controller):** Enhance `registerUserCtrl` to handle user registration efficiently. Check for existing users, hash the password using bcrypt, and create a new user in the database. Respond with a status of 201 for successful registration along with user details.

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -1,7 +1,33 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+
 
 export const registerUserCtrl = async(req, res) => {
-    res.json({
-        msg:"User register controller",
+    const {fullname, email, password} = req.body;
+
+    // * Check if user exist
+    const userExists = await User.findOne({email});
+    if (userExists) {
+        res.json({
+            msg: "User already exists⚠",
+        });
+    }
+
+    // -> HASHING
+    const salt = await bcrypt.genSalt(9);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    // @ Create User, Hash user password
+    const user = await User.create({
+        fullname,
+        email,
+        password: hashPassword
+    });
+
+    res.status(201).json({
+        status:'success',
+        message: 'User Registered Successfully✅',
+        data:user
     });
 };
