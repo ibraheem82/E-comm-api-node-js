49c4d27753208458243f2e0584b16d81bb021841
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-13 15:58:19 +0100

    notFound was imported... to be use to detect if the the url is wrong...

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ import express from 'express';
 
 import dbConnect from '../config/dbConnect.js';
 
-import { globalErrHandler } from '../middlewares/globalErrHandler.js';
+import { globalErrHandler, notFound } from '../middlewares/globalErrHandler.js';
 import userRoutes from '../routes/usersRoute.js';
 
 // * Database Connection.
@@ -19,6 +19,7 @@ app.use('/', userRoutes);
 
 
 // * Err Middleware
+app.use(notFound);
 app.use(globalErrHandler);
 
 export default app;
