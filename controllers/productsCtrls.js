43ba36e19d1f9d3cd5a6b07f610216f25ca04e76
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-14 07:57:55 +0100

    **feat(controller):** Implement `createProductCtrl` controller to handle the creation of a product. Check if the product already exists, and if not, create a new product using the provided information. Respond with a status of success, a message indicating successful creation, and details of the created product.

--- controllers/productsCtrls.js
+++ controllers/productsCtrls.js
@@ -1,9 +1,36 @@
 import asyncHandler from "express-async-handler";
 import Product from "../models/Product.js";
 
+// @desc Create new Product
+// @route POST /api/v1/products
+// @access Private/Admin
 
+export const createProductCtrl = asyncHandler(async(req, res) => {
+    const {name, description, category, sizes, colors, user, price, totalQty, brand} = req.body;
 
+    const productExist = await Product.findOne({name});
+    if(productExist){
+        throw new Error("Product already exist⚠")
+    }
 
+    const product = await Product.create({
+        name,
+         description, 
+         category,
+          sizes,
+          colors, 
+          user : req.userAuthId,
+          price,
+          totalQty,
+          brand,
+    });
+
+    res.json({
+        status:'success',
+        message: 'Product created Successfully✅',
+        product
+    });
+});
 
 
 
