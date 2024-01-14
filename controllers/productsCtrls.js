10bf7309a2c33ec3e771614b3cc5ab99b45727f7
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-14 10:03:20 +0100

    **feat(controller):** Extend product filtering in the `getProductsCtrl` controller to include additional query parameters such as `brand`, `category`, `color`, and `size`. If any of these parameters are provided in the request, filter products accordingly. Each filter performs a case-insensitive regex match, and the final set of products is returned in the response.

--- controllers/productsCtrls.js
+++ controllers/productsCtrls.js
@@ -40,12 +40,44 @@ export const createProductCtrl = asyncHandler(async(req, res) => {
 export const getProductsCtrl = asyncHandler(async(req, res) => {
     let productQuery = Product.find();
 
-    // * SEARCHING PRODUCTS.
+    // * SEARCHING PRODUCTS BY NAME.
     if(req.query.name){
         productQuery = productQuery.find({
             name:{ $regex: req.query.name, $options:"i" },
         });
     }
+
+
+    // * filter PRODUCTS BY BRAND.
+    if(req.query.brand){
+        productQuery = productQuery.find({
+            brand:{ $regex: req.query.brand, $options:"i" },
+        });
+    }
+
+
+    // * filter by category
+    if(req.query.category){
+        productQuery = productQuery.find({
+            category:{ $regex: req.query.category, $options:"i" },
+        });
+    }
+
+
+        // * filter by color
+    if(req.query.color){
+        productQuery = productQuery.find({
+            colors:{ $regex: req.query.color, $options:"i" },
+        });
+    }
+
+    // * filter by size
+    if(req.query.size){
+        productQuery = productQuery.find({
+            sizes:{ $regex: req.query.size, $options:"i" },
+        });
+    }
+
     const products = await productQuery;
 
     res.json({
