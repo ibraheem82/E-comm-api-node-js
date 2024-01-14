e82e7bef7a4cfd3c039d4416c20759ea5ccb618b
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-14 07:55:19 +0100

    **feat(app):** Mount product routes under the `/api/v1/products/` path in the Express application, directing requests to the endpoints defined in `productRouter`.

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ import dbConnect from '../config/dbConnect.js';
 
 import { globalErrHandler, notFound } from '../middlewares/globalErrHandler.js';
 import userRoutes from '../routes/usersRoute.js';
+import productRouter from "../routes/productsRoute.js";
 
 // * Database Connection.
 dbConnect();
@@ -16,6 +17,7 @@ app.use(express.json());
 
 // @Routes
 app.use('/api/v1/users/', userRoutes);
+app.use('/api/v1/products/', productRouter);
 
 
 // * Err Middleware
