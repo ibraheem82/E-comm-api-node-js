b3a395de135b28b8559bb322f05f61fe113be32c
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-01-31 14:03:17 +0100

    ....

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -9,6 +9,6 @@ productRouter.post('/', isLoggedIn, createProductCtrl);
 productRouter.get('/', getProductsCtrl);
 productRouter.get('/:id', getProductCtrl);
 productRouter.put('/:id', isLoggedIn, updateProductCtrl);
-productRouter.delete('/:id', isLoggedIn, deleteProductCtrl);
+productRouter.delete('/:id/delete', isLoggedIn, deleteProductCtrl);
 
 export default productRouter;
