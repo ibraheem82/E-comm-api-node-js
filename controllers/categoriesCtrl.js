e80cadbfc870025940ab3f4e1322edf90e566961
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-02-03 08:37:54 +0100

    feat: Implement category CRUD operations
    This commit introduces controllers for handling CRUD (Create, Read, Update, Delete) operations on categories in the API. The controllers include:
    
    - `getAllCategoriesCtrl`: Fetches all categories.
    - `getSingleCategoryCtrl`: Fetches a single category by ID.
    - `updateCategoryCtrl`: Updates a category by ID.
    - `deleteCategoryCtrl`: Deletes a category by ID.
    
    Each controller responds with a JSON object containing the status, a message indicating the operation's success, and relevant data (categories or updated/deleted category).

--- controllers/categoriesCtrl.js
+++ controllers/categoriesCtrl.js
@@ -25,3 +25,74 @@ export const createCategoryCtrl = asyncHandler(async(req, res) => {
         category,
     })
 })
+
+
+
+// @desc Get all categories
+// @route GET /api/categories
+// @access Public
+
+
+export const getAllCategoriesCtrl = asyncHandler(async(req, res) => {
+    const categories = await Category.find();
+
+    res.json({
+        status:"success",
+        message: "Categories fetched",
+        categories,
+    });
+});
+
+
+// @desc Get single category
+// @route GET /api/categories/:id
+// @access Public
+
+export const getSingleCategoryCtrl = asyncHandler(async(req, res) => {
+    const category = await Category.findById(req.params.id);
+
+    res.json({
+        status:"success",
+        message: "Category fetched",
+        category,
+    });
+});
+
+
+
+// @desc Update Category
+// @route PUT /api/categories/:id/update
+// @access Pivate/admin
+
+export const updateCategoryCtrl = asyncHandler(async(req, res) => {
+    const {name} = req.body;
+
+    const category = await Category.findByIdAndUpdate(req.params.id,
+         {
+         name
+    }, {
+        new: true,
+    }
+    );
+    
+    res.json({
+        status: "success",
+        message: "Category updated successfully✅",
+        category
+    });
+});
+
+
+
+
+// @desc Delete Category
+// @route GET /api/categories/:id/delete
+// @access Pivate/admin
+
+export const deleteCategoryCtrl = asyncHandler(async(req, res) => {
+    await Category.findByIdAndDelete(req.params.id);
+       res.json({
+           status: "success",
+           message: "Category deleted successfully✅",
+       });
+   })
