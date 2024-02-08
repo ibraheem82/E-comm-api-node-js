e81e87157a0ad09efc2f35e247d7a60dca4c2efd
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-02-09 00:14:46 +0100

    feat: Implement Brand CRUD operations
    This commit implements CRUD (Create, Read, Update, Delete) operations for the Brand model. It includes the following changes:
    
    1. Creation of a new brand: A POST endpoint is added to create a new brand. It checks if the brand already exists before creating a new one.
    
    2. Retrieval of all brands: A GET endpoint is added to fetch all brands from the database.
    
    3. Retrieval of a single brand: A GET endpoint is added to fetch a single brand based on its ID.
    
    4. Updating a brand: A PUT endpoint is added to update the details of a brand based on its ID.
    
    5. Deleting a brand: A DELETE endpoint is added to delete a brand based on its ID.
    
    These endpoints handle the corresponding CRUD operations for managing brands in the application.

---
+++ controllers/brandCtrl.js
@@ -0,0 +1,94 @@
+import asyncHandler from "express-async-handler";
+import Brand from "../models/Brand.js";
+
+// @desc Create new brand
+// @route POST /api/v1/brand
+// @access Pivate/admin
+export const createBrandCtrl = asyncHandler(async(req, res) => {
+    const {name} = req.body;
+    const brandFound = await Brand.findOne({name})
+    if(brandFound){
+        throw new Error('Brand already exists⚠')
+    }
+
+    // create
+    const brand = await Brand.create({
+        name: name.toLowerCase(),
+        // attaching user to it.
+        user: req.userAuthId,
+    })
+
+    res.json({
+        status:"success",
+        message: "Brand created successfully",
+        brand,
+    })
+})
+
+
+
+// @desc Get all brands
+// @route GET /api/brands
+// @access Public
+export const getAllBrandsCtrl = asyncHandler(async(req, res) => {
+    const brands = await Brand.find();
+
+    res.json({
+        status:"success",
+        message: "Brands fetched",
+        brands,
+    });
+});
+
+
+// @desc Get single brands
+// @route GET /api/brands/:id
+// @access Public
+
+export const getSingleBrandCtrl = asyncHandler(async(req, res) => {
+    const brands = await Brand.findById(req.params.id);
+
+    res.json({
+        status:"success",
+        message: "Brand fetched",
+        brands,
+    });
+});
+
+
+
+// @desc Update Brand
+// @route PUT /api/brands/:id
+// @access Private/admin
+
+export const updateCtrl = asyncHandler(async(req, res) => {
+    const {name} = req.body;
+
+    const brand = await Brand.findByIdAndUpdate(req.params.id,
+         {
+         name
+    }, {
+        new: true,
+    }
+    );
+    
+    res.json({
+        status: "success",
+        message: "Brand updated successfully✅",
+        brand
+    });
+});
+
+
+
+
+// @desc Delete Brand
+// @route GET /api/brands/:id
+// @access Pivate/admin
+export const deleteBrandCtrl = asyncHandler(async(req, res) => {
+    await Brand.findByIdAndDelete(req.params.id);
+       res.json({
+           status: "success",
+           message: "Brand deleted successfully✅",
+       });
+})
