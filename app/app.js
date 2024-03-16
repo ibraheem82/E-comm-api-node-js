a2b7dfeb8517427d72f273b124b5a36faf7697bc
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-03-16 03:46:36 +0100

    review route was mount in the app...

--- app/app.js
+++ app/app.js
@@ -10,7 +10,7 @@ import productRouter from "../routes/productsRoute.js";
 import categoriesRouter from "../routes/categoriesRouter.js";
 import brandsRouter from "../routes/brandsRouter.js";
 import colorRouter from "../routes/colorRouter.js";
-
+import reviewRouter from "../routes/reviewRouter.js";
 
 // * Database Connection.
 dbConnect();
@@ -25,6 +25,7 @@ app.use('/api/v1/products/', productRouter);
 app.use('/api/v1/categories/', categoriesRouter);
 app.use('/api/v1/brands/', brandsRouter);
 app.use('/api/v1/colors/', colorRouter);
+app.use('/api/v1/reviews/', reviewRouter);
 
 
 // * Err Middleware
