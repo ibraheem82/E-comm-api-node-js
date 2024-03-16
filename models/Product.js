9451ef0401b8721682f9a5906fe0de514a97a57d
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-03-16 12:36:22 +0100

    added more comments for easy understanding of codebase..

--- models/Product.js
+++ models/Product.js
@@ -80,28 +80,49 @@ const ProductSchema = new Schema(
   }
 );
 
+/**
+ * What are Virtuals?
+
+Virtuals are fields that are not saved directly in your MongoDB database collection.
+They are calculated or derived dynamically when you access them from a document.
+They offer flexibility by allowing you to compute values on the fly without storing redundant data.
+ */
 
 // Virtuals
 // Total rating
 
+/**
+ * Purpose: Computes the total number of reviews associated with a product.
+Logic:
+(this) inside the function refers to the current product document.
+It accesses the reviews array (populated from the relationship defined earlier) and gets its length.
+ */
 ProductSchema.virtual('totalReviews').get(function(){
 const product = this;
 return product?.reviews?.length;
 });
 
 // Average rating
-ProductSchema.virtual('averageRating').get(function(){
 
+
+/** *
+ * Purpose: Calculates the average rating of a product based on its reviews.
+Logic:
+It iterates over the reviews array, summing up the rating of each review.
+It calculates the average by dividing the total rating by the number of reviews.
+.toFixed(1) formats the average to one decimal place.
+ */
+ProductSchema.virtual('averageRating').get(function(){
   let ratingsTotal = 0;
   const product = this;
   product?.reviews?.forEach((review) => {
     ratingsTotal += review?.rating
   });
-
   // Calculate average rating.
   const averageRating = Number(ratingsTotal / product?.reviews?.length).toFixed(1);
   return averageRating;
 });
 
+// * creates a Mongoose model named "Product" based on your ProductSchema.
 const Product = mongoose.model("Product", ProductSchema);
 export default Product
