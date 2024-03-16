e5ff6716f4ef417db642a5d7f9eb97634b20a782
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-03-16 12:32:10 +0100

    Add Virtuals to Product Schema
    - Implemented virtuals for calculating total reviews and average rating of a product
    - Added virtual fields to the Product schema to dynamically calculate total reviews and average rating based on associated reviews

--- models/Product.js
+++ models/Product.js
@@ -1,13 +1,15 @@
 //product schema
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
-
+// Mongoose schema called ProductSchema
 const ProductSchema = new Schema(
   {
     name: {
       type: String,
       required: true,
     },
+
+    
     description: {
       type: String,
       required: true,
@@ -16,6 +18,8 @@ const ProductSchema = new Schema(
       type: String,
       required: true,
     },
+
+    // ! ref: "Category" Establishes a relationship between the product and a likely separate "Category" model
     category: {
       type: String,
       ref: "Category",
@@ -23,6 +27,7 @@ const ProductSchema = new Schema(
     },
     sizes: {
       type: [String],
+      // ! Restricts the valid values for sizes to only those specified in the list.
       enum: ["S", "M", "L", "XL", "XXL"],
       required: true,
     },
@@ -32,6 +37,7 @@ const ProductSchema = new Schema(
     },
 
     user: {
+      // Establishes a relationship between the product and a "User" model, storing a user's ObjectID.
       type: mongoose.Schema.Types.ObjectId,
       required: true,
       ref: "User",
@@ -67,10 +73,35 @@ const ProductSchema = new Schema(
     },
   },
   {
+    //* Tells Mongoose to automatically add createdAt and updatedAt fields to your documents, tracking when they were created and last modified.
     timestamps: true,
+    // Configures how your schema is converted into JSON. The virtuals: true part ensures that any virtual fields you might define (not present in the database) are included in the JSON output.
     toJSON: { virtuals: true },
   }
 );
+
+
+// Virtuals
+// Total rating
+
+ProductSchema.virtual('totalReviews').get(function(){
+const product = this;
+return product?.reviews?.length;
+});
+
+// Average rating
+ProductSchema.virtual('averageRating').get(function(){
+
+  let ratingsTotal = 0;
+  const product = this;
+  product?.reviews?.forEach((review) => {
+    ratingsTotal += review?.rating
+  });
+
+  // Calculate average rating.
+  const averageRating = Number(ratingsTotal / product?.reviews?.length).toFixed(1);
+  return averageRating;
+});
 
 const Product = mongoose.model("Product", ProductSchema);
 export default Product
