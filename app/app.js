99d7a5c8d04513d7de2e88649a974c815b1fc060
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-03-16 13:11:30 +0100

    orderRouter, routes was imported and mount so as to access the route.

--- app/app.js
+++ app/app.js
@@ -11,6 +11,7 @@ import categoriesRouter from "../routes/categoriesRouter.js";
 import brandsRouter from "../routes/brandsRouter.js";
 import colorRouter from "../routes/colorRouter.js";
 import reviewRouter from "../routes/reviewRouter.js";
+import orderRouter from "../routes/ordersRouter.js";
 
 // * Database Connection.
 dbConnect();
@@ -26,6 +27,7 @@ app.use('/api/v1/categories/', categoriesRouter);
 app.use('/api/v1/brands/', brandsRouter);
 app.use('/api/v1/colors/', colorRouter);
 app.use('/api/v1/reviews/', reviewRouter);
+app.use('/api/v1/orders/', orderRouter);
 
 
 // * Err Middleware
